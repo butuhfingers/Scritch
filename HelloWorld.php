5f21614f2ce77f340597db4f6ede8def8e8d81c7
derek <derek.crew@rocky.edu>
2016-09-26 20:02:03 -0600

    Hello World Created

---
+++ HelloWorld.php
@@ -0,0 +1,16 @@
+<?php
+
+$hello = new HelloWorld();
+
+/**
+ * Created by PhpStorm.
+ * User: derek
+ * Date: 9/26/16
+ * Time: 1:56 PM
+ */
+class HelloWorld
+{
+    public function HelloWorld(){
+        echo("Hello World");
+    }
+}
