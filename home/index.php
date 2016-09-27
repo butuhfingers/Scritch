1355c0c5b82376fdac237af254a498e838f222a1
derek <derek.crew@rocky.edu>
2016-09-26 20:46:27 -0600

    Redirect to home

---
+++ home/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: derek
+ * Date: 9/26/16
+ * Time: 8:45 PM
+ */
+echo("Home");
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: derek
+ * Date: 9/26/16
+ * Time: 8:29 PM
+ */
+//Redirect the browser
+header("Location: /hoome/");
+exit();
